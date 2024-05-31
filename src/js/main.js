13280b99171bad7a0e74cea542288d7869b5dda6
SimonArias77 <sariass077@gmail.com>
2024-05-31 09:20:34 -0500

    fix:se culmina estructura de carpetas e importaciones

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,16 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+import Swal from 'sweetalert2'
+
+const btn = document.querySelector("#btn")
+
+btn.addEventListener("click", () => {
+    Swal.fire({
+        title: 'Error!',
+        text: 'hi simòn',
+        icon: 'error',
+        confirmButtonText: 'Cool'
+    })
+});
